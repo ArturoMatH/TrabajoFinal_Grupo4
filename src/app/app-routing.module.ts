cca98b05de00af0eb7275821e6c2dcb17c66a624
ArturoMatH <89763723+ArturoMatH@users.noreply.github.com>
2021-09-01 18:56:43 -0500

    Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,13 @@ import { RouterModule, Routes } from "@angular/router";
 import { HomeComponent } from "./pages/home/home.component";
 import { RegistroComponent } from "./pages/registro/registro.component";
 import { CestaComponent } from "./pages/cesta/cesta.component";
+import { AboutusComponent } from "./pages/aboutus/aboutus.component";
+import { ContactusComponent } from "./pages/contactus/contactus.component";
 
 const routes: Routes = [
     { path: 'home', component: HomeComponent},
+    { path: 'aboutus', component: AboutusComponent},
+    { path: 'contacus', component: ContactusComponent},
     { path: 'registro', component: RegistroComponent },
     { path: 'cesta', component: CestaComponent}
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,17 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { RegistroComponent } from './pages/registro/registro.component';
 import { CestaComponent } from './pages/cesta/cesta.component';
+import { ContactusComponent } from './pages/contactus/contactus.component';
+import { AboutusComponent } from './pages/aboutus/aboutus.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     RegistroComponent,
-    CestaComponent
+    CestaComponent,
+    ContactusComponent,
+    AboutusComponent
   ],
   imports: [
     BrowserModule,
